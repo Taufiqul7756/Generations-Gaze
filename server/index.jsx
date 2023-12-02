ef8369e60fd8df31d1848b320343d42630b7eba6
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-02 14:41:31 +0600

    Initial commit - add server folder

---
+++ server/index.jsx
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+//middleware
+app.use(express.json()); //req body
+app.use(cors());
+
+//ROUTES
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
