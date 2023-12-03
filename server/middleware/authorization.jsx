445e26f887146e7338a0f0669993cb7036eea1ce
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-03 22:41:58 +0600

    next() - missing , right spelling token name

--- server/middleware/authorization.jsx
+++ server/middleware/authorization.jsx
@@ -4,12 +4,13 @@ require("dotenv").config();
 module.exports = async (req, res, next) => {
   try {
     const jwtToken = req.header("token");
-    if (!token) {
+    if (!jwtToken) {
       return res.status(403).json("Not Authorize");
     }
 
     const payload = jwt.verify(jwtToken, process.env.jwtSecret);
     req.user = payload.user;
+    next();
   } catch (err) {
     console.error(err.message);
     return res.status(403).json("Not Authorize ");
