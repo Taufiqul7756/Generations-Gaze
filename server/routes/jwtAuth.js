eb4be74eee616423e78a2f701cc7e3b5527a813b
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-03 18:48:20 +0600

    Login route and token for user

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -37,4 +37,34 @@ router.post("/register", async (req, res) => {
     res.status(500).send("server Error");
   }
 });
+
+//login---------------
+router.post("/login", async (req, res) => {
+  try {
+    //1. destructure req.body
+    const { email, password } = req.body;
+    //2. check if user doesn't exist ( if not throw error)
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+      email,
+    ]);
+    if (user.rows.length === 0) {
+      return res.status(401).json("Password or email is incorrect");
+    }
+    //3. check if incoming password is the same the database password
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].user_password
+    );
+    if (!validPassword) {
+      return res.status(401).json("Password or email is incorrect");
+    }
+
+    //4. give them the jwt token
+    const token = jwtGenerator(user.rows[0].user_id);
+    res.json({ token });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server Error");
+  }
+});
 module.exports = router;
