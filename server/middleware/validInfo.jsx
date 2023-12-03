5239f1b48f73818fb760cff2da5850c3f53864b6
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-03 20:15:29 +0600

    name , email Validation -

--- server/middleware/validInfo.jsx
+++ server/middleware/validInfo.jsx
@@ -1,9 +1,9 @@
 module.exports = function (req, res, next) {
   const { email, name, password } = req.body;
 
-  function validEmail(userEmail) {
+  const validEmail = (userEmail) => {
     return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
-  }
+  };
 
   if (req.path === "/register") {
     console.log(!email.length);
--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -3,9 +3,10 @@ const router = require("express").Router();
 const pool = require("../db.jsx");
 const bcrypt = require("bcrypt");
 const jwtGenerator = require("../utils/jwtGenerator.js");
+const validInfo = require("../middleware/validInfo.jsx");
 
 //registering
-router.post("/register", async (req, res) => {
+router.post("/register", validInfo, async (req, res) => {
   try {
     //1. destructure the req.body
     const { name, email, password } = req.body;
@@ -39,7 +40,7 @@ router.post("/register", async (req, res) => {
 });
 
 //login---------------
-router.post("/login", async (req, res) => {
+router.post("/login", validInfo, async (req, res) => {
   try {
     //1. destructure req.body
     const { email, password } = req.body;
