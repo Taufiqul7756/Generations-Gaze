a15e8e317f5a442f7aba258283bf3a64485a541c
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-04 00:28:36 +0600

    dashboard routes - added

--- server/index.jsx
+++ server/index.jsx
@@ -11,6 +11,9 @@ app.use(cors());
 //Register and login routes
 app.use("/auth", require("./routes/jwtAuth"));
 
+//dashboard route
+app.use("/dashboard", require("./routes/dashboard"));
+
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
