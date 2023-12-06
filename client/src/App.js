6aa238f15911fc3d93375ecf521c3e9c4b0d1158
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-06 15:18:04 +0600

    Routing - login , register and dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
 
 import Dashboard from "./components/Dashboard.jsx";
 import Login from "./components/Login.jsx";
@@ -9,25 +15,46 @@ import "./App.css";
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  };
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
           <Route
             path="/login"
-            element={isAuthenticated ? <Navigate to="/dashboard" /> : <Login />}
+            element={
+              isAuthenticated ? (
+                <Navigate to="/dashboard" />
+              ) : (
+                <Login setAuth={setAuth} />
+              )
+            }
           />
           <Route
             path="/register"
-            element={isAuthenticated ? <Navigate to="/login" /> : <Register />}
+            element={
+              isAuthenticated ? (
+                <Navigate to="/dashboard" />
+              ) : (
+                <Register setAuth={setAuth} />
+              )
+            }
           />
           <Route
             path="/dashboard"
-            element={isAuthenticated ? <Navigate to="/dashboard" /> : <Login />}
+            element={
+              isAuthenticated ? (
+                <Dashboard setAuth={setAuth} />
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
           />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   );
 };
