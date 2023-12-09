3d65523b41e097badf6f8db8de8209a510d083ef
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-09 20:19:03 +0600

    Login compo - 80%

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,17 +2,70 @@ import React from "react";
 
 const Login = ({ setAuth }) => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Login</h1>
-
-      <button
-        onClick={() => setAuth(true)}
-        type="button"
-        class="py-2.5 px-5 me-2 mt-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-      >
-        Authenticate
-      </button>
-    </div>
+    <>
+      <h1 className="flex justify-center font-bold "> Login</h1>
+      <div>
+        <div className="flex justify-center ">
+          <form class="w-full max-w-lg">
+            <div class="w-full px-3">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-first-name"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                placeholder="name@gmail.com"
+                required
+              />
+            </div>
+            <div class="w-full px-3">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-password"
+              >
+                Password
+              </label>
+              <input
+                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                id="grid-password"
+                type="password"
+                placeholder="******************"
+              />
+              <p class="text-gray-600 text-xs italic">
+                Make it as long and as crazy as you'd like
+              </p>
+            </div>
+            <div class="flex items-start mb-5">
+              <div class="flex items-center h-5">
+                <input
+                  id="remember"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                  required
+                />
+              </div>
+              <label
+                for="remember"
+                class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Remember me
+              </label>
+            </div>
+          </form>
+        </div>
+        <button
+          onClick={() => setAuth(true)}
+          class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >
+          Sign in
+        </button>
+      </div>
+    </>
   );
 };
 
