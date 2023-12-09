699181054018e8d93e1dc5c355fca3a9185993b8
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-10 02:11:36 +0600

    Dashboard - 10%

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -6,8 +6,7 @@ const Dashboard = ({ setAuth }) => {
       <h1 className="text-3xl font-bold underline">Dashboard</h1>
       <button
         onClick={() => setAuth(false)}
-        type="button"
-        class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
       >
         Logout
       </button>
