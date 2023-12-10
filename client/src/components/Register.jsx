e65eece6a44e99f7697ad740832b8966b10ce8de
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-10 14:53:14 +0600

    jwToken generated - POST Method

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,78 +1,115 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { useState } from "react";
 
 const Register = ({ setAuth }) => {
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+    name: "",
+  });
+  const { email, password, name } = inputs;
+  const onChange = (e) =>
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { name, email, password };
+      const response = await fetch("http://localhost:5000/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      const parseRes = await response.json();
+      console.log(parseRes);
+      if (parseRes.authenticated) {
+        setAuth(true);
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
   return (
     <>
       <h1 className="flex justify-center font-bold "> Register</h1>
       <div className="flex justify-center ">
-        <form class="w-full max-w-lg">
-          <div class="w-full px-3">
+        <form onSubmit={onSubmitForm} className="w-full max-w-lg">
+          <div className="w-full px-3">
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-first-name"
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-first-name"
             >
               Name
             </label>
             <input
-              class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
               id="grid-first-name"
               type="text"
+              name="name"
               placeholder="Your Name"
+              value={name}
+              onChange={onChange}
               required
             />
           </div>
-          <div class="w-full px-3">
+          <div className="w-full px-3">
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-first-name"
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-first-name"
             >
               Email
             </label>
             <input
               type="email"
               id="email"
-              class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-              placeholder="name@flowbite.com"
+              name="email"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              placeholder="name@gmail.com"
+              value={email}
+              onChange={onChange}
               required
             />
           </div>
-          <div class="w-full px-3">
+          <div className="w-full px-3">
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-password"
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-password"
             >
               Password
             </label>
             <input
-              class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
               id="grid-password"
               type="password"
+              name="password"
               placeholder="******************"
+              value={password}
+              onChange={onChange}
             />
-            <p class="text-gray-600 text-xs italic">
+            <p className="text-gray-600 text-xs italic">
               Make it as long and as crazy as you'd like
             </p>
           </div>
 
-          <div class="flex items-start mb-5">
-            <div class="flex items-center h-5">
+          <div className="flex items-start mb-5">
+            <div className="flex items-center h-5">
               <input
                 id="terms"
                 type="checkbox"
                 value=""
-                class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
                 required
               />
             </div>
             <label
-              for="terms"
-              class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              htmlFor="terms"
+              className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
               I agree with the{" "}
               <a
                 href="#"
-                class="text-blue-600 hover:underline dark:text-blue-500"
+                className="text-blue-600 hover:underline dark:text-blue-500"
               >
                 terms and conditions
               </a>
@@ -80,8 +117,8 @@ const Register = ({ setAuth }) => {
           </div>
           <button
             type="submit"
-            onClick={() => setAuth(true)}
-            class=" bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            // onClick={() => setAuth(true)}
+            className=" bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
           >
             Sign up
           </button>
