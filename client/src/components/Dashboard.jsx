8fe4776c8623957e6ad6570e209a8df3c8eedb12
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-10 22:37:42 +0600

    class to className - text DOM manipulation

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -6,7 +6,7 @@ const Dashboard = ({ setAuth }) => {
       <h1 className="text-3xl font-bold underline">Dashboard</h1>
       <button
         onClick={() => setAuth(false)}
-        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
       >
         Logout
       </button>
