3813741e3c6310c242b063e2d34309fc216d6b69
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-03-16 22:40:23 +0600

    remove token from localStorage when logout button clicked

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -9,11 +9,16 @@ const Dashboard = ({ setAuth }) => {
       .then((data) => setUsers(data))
       .catch((error) => console.log("Error Fetching User: ", error));
   }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setAuth(false);
+  };
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Dashboard</h1>
       <button
-        onClick={() => setAuth(false)}
+        onClick={handleLogout}
         className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
       >
         Logout
